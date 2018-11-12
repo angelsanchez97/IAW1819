368e0ca597885dacc04d7b910249cb4e39d92036
Alumno <Alumno@AULA29-02>
2018-11-12 10:49:32 +0100

    a

---
+++ Exepciones/src/ejercicios/Teoria.java
@@ -0,0 +1,18 @@
+package ejercicios;
+
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+public class Teoria {
+	private static Scanner sc = new Scanner(System.in);
+	public static void main(String[] args) {
+		
+//			int valor = 100/0;
+//			System.out.println(valor);
+			int num = Leer.datoInt();
+			System.out.println(num);
+		
+
+	}
+
+}
